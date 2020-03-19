38f4069e092a66da10df9e4a7df7a8e5b5e074d2
Shaikhatarov, Artur <AShaikhatarov@luxoft.com>
2020-03-20 01:05:59 +0300

    started inference

--- examples/inference.py
+++ examples/inference.py
@@ -1,7 +1,8 @@
 import torch
+from facenet_pytorch import InceptionResnetV1
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = torch.load('aerialmodel.pth')
-model.eval()
-
+model = InceptionResnetV1()
+model.load_state_dict(torch.load('model_best.pth.tar')['state_dict'])
+print("final")
